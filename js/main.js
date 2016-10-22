2eefec4d38b6cf34bc69b98ee0516639986a1f2d
sashakozakov <sashakozakov2293@gmail.com>
2016-10-22 14:59:00 +0300

    home page desctop

---
+++ js/main.js
@@ -0,0 +1,28 @@
+;
+(function ($) {
+	$(document).ready(function () {
+		var $slider = $('.home_slider');
+		// var $slider = $('.slider-image');
+		var $testimonials = $('.testimonial-slider');
+
+		$slider.slick({
+			dots: true,
+			infinite: true,
+			speed: 500,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			fade: true,
+			cssEase: 'linear',
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: true,
+			nextArrow: '<i class="fa fa-chevron-right next" aria-hidden="true"></i>',
+			prevArrow: '<i class="fa fa-chevron-left prev" aria-hidden="true"></i>',
+			slide: '.slide'
+		});
+
+		$('.home_slider').find('.slick-dots').appendTo('.home_slider .caption .custom_dots');
+
+
+	});
+}(jQuery));
