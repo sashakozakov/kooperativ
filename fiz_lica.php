7fdf9928483d90678885f2a875046964b0730c96
sashakozakov <sashakozakov2293@gmail.com>
2016-10-23 20:59:29 +0300

    added fiz lica page

---
+++ fiz_lica.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Главная</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800|PT+Sans:400,700" rel="stylesheet">
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/fonts.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/jquery.mCustomScrollbar.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/media.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body id="top" class="lica lica_fiz">
+
+	<header class="header">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="logo">
+					<a href="#">
+						<img src="images/logo.png" alt="kooperativ">
+					</a>
+				</div>
+
+				<a href="tel:84997559734" class="header_tel">8 (499) 755-97-34</a>
+			</div>
+		</div>
+	</header>
+
+	<section class="home_slider fiz_slider">
+		<div class="slide" style="background-image:url('images/slide1.jpg')">
+			<div class="row">
+				<div class="caption">
+				<div class="icon">
+					<img src="images/fiz_icon_slider.png" alt="icon name">
+				</div>
+					<h1>для</h1>
+					<h1>Физических лиц</h1>
+					<div class="line"></div>
+					<h5>Выгоды для физических лиц при вступлении в
+						ПО МКХ «ДОВЕРИЕ»</h5>
+					<div class="custom_dots"></div>
+				</div>
+			</div>
+		</div>
+		<div class="slide" style="background-image:url('images/slide2.jpg')">
+			<div class="row">
+				<div class="caption">
+				<div class="icon">
+					<img src="images/fiz_icon_slider.png" alt="icon name">
+				</div>
+					<h1>для</h1>
+					<h1>Физических лиц</h1>
+					<div class="line"></div>
+					<h5>Выгоды для физических лиц при вступлении в
+						ПО МКХ «ДОВЕРИЕ»</h5>
+					<div class="custom_dots"></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="banner_buttons">
+			<div class="row">
+				<div class="button_block button_block_1 fiz_lica_btn">
+					<div class="content">
+						<h3>Основные практические моменты выгоды
+							заключаются в следующем:</h3>
+					</div>
+				</div>
+				<a href="#" class="button_block button_block_2">
+					<div class="bg"></div>
+					<div class="content">
+						<img src="images/ur_lic.png" alt="Юридическим лицам">
+						<h3>Юридическим лицам</h3>
+					</div>
+				</a>
+			</div>
+		</div>
+	</section>
+
+	<section class="fiz_ur_content fiz_cont">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="list_cont">
+					<p>
+						Потребительское общество создано для удовлетворения потребностей его пайщиков, то есть вступивших в ПО МКХ. За относительно невысокую сумму, вносимую при вступлении: 100 рублей – вступительный взнос (при выходе из Общества не возвращается) и 500 рублей – минимальный паевой взнос (при выходе из Общества подлежит возврату) пайщик получает практически неограниченный перечень льгот и выгод для себя!
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="row features_grid">
+			<div class="col-sm-12">
+				<ul>
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/pen.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										Удовлетворение любых материальных потребностей пайщика.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/legal-file-document.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										Законодательная гарантия сохранности паевого взноса в
+										виде: денежных средств, имущества, интеллектуальной
+										собственности.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/donation-box.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										Пайщики не обязаны ни перед кем обосновывать происхождение своего паевого взноса.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/bill-with-dollar-sign-and-coins.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										На паевые взносы не распространяются судебные решения,
+										раздел имущества, арест, завладение имуществом 3-х лиц.
+										Кредиторы и судебные приставы не могут изъять паевой
+										взнос у пайщика.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/court-hammer.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										На паевой взнос пайщика не обращаются иски по
+										долгам пайщика, как физического лица.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/indent-decrease-symbol.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										Паевые взносы не декларируются. При возврате паевого
+										взноса (внесение в виде увеличения и возврат в виде
+										уменьшения) не облагается НДФЛ и не декларируется.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/share.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										При вступлении в ПО МКХ «ДОВЕРИЕ» пайщик получает
+										возможность участия непосредственно в хозяйственной
+										деятельности Общества «1 пайщик – 1 голос».
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="matchHeight">
+						<div class="feature_block">
+							<div class="image matchHeight_text">
+								<div class="img">
+									<?php echo file_get_contents('images/technical-support.svg'); ?>
+								</div>
+							</div>
+							<div class="content matchHeight_text">
+								<div>
+									<p>
+										Пайщик получает возможность пользоваться на льготных
+										условиях услугами, предоставляемыми пайщиками
+										Общества, и потреблять товары из паевого фонда Общества.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+				</ul>
+			</div>
+		</div>
+	</section>
+
+
+
+
+	<section class="mkh_doverie">
+		<div class="row text-center">
+			<h2 class="section_title">ПО МКХ «ДОВЕРИЕ»</h2>
+			<h4 class="title_subtitle">Существуют такие понятия, как</h4>
+			<ul class="section_title_dots">
+				<li></li><li></li><li></li>
+			</ul>
+
+			<div class="block_container">
+				<div class="block matchHeight_text">
+					<a href="#">
+						<div class="inner_block">
+							<div>
+								<?php echo file_get_contents('images/icon.svg'); ?>
+							</div>
+						</div>
+						<h5 class="title">Кооперативные выплаты по итогам работы за год</h5>
+					</a>
+				</div>
+				<div class="block matchHeight_text">
+					<a href="#">
+						<div class="inner_block">
+							<div>
+								<?php echo file_get_contents('images/dollar-sign-and-piles-of-coins.svg'); ?>
+							</div>
+						</div>
+						<h5 class="title">Беззалоговое финансирование пайщиков</h5>
+					</a>
+				</div>
+				<div class="block matchHeight_text">
+					<a href="#">
+						<div class="inner_block">
+							<div>
+								<?php echo file_get_contents('images/hand-shake.svg'); ?>
+							</div>
+						</div>
+						<h5 class="title">Материальная помощь пайщикам</h5>
+					</a>
+				</div>
+				<div class="block matchHeight_text">
+					<a href="#">
+						<div class="inner_block">
+							<div>
+								<?php echo file_get_contents('images/dangerous-zone.svg'); ?>
+							</div>
+						</div>
+						<h5 class="title">Беззалоговое потребительское кредитование пайщиков</h5>
+					</a>
+				</div>
+				<div class="block matchHeight_text">
+					<a href="#">
+						<div class="inner_block">
+							<div>
+								<?php echo file_get_contents('images/mortgage.svg'); ?>
+							</div>
+						</div>
+						<h5 class="title">Безвозмездное получение имущества в пользование</h5>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="fiz_form">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="section_title text-center">ФОРМА ЗАЯВКИ</h2>
+				<ul class="section_title_dots">
+					<li></li><li></li><li></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<form action="#" method="post">
+					<input type="text" id="surname" placeholder="Фамилия">
+					<input type="text" id="name" placeholder="Имя">
+					<input type="text" id="otch" placeholder="Отчество">
+					<div class="width-1-2">
+						<label>Дата рождения</label>
+						<input type="date" id="date">
+					</div>
+					<div class="width-1-2">
+						<label>Номер паспорта</label>
+						<input type="text" id="passport" placeholder="№">
+					</div>
+					<input type="tel" id="tel" placeholder="Телефон">
+					<input type="email" id="email" placeholder="E-mail">
+					<textarea placeholder="Текст"></textarea>
+					<div class="text-center">
+						<input type="submit" value="Подать заявку">
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
+
+
+	<footer class="footer">
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="social text-center">
+					<li>
+						<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-12 text-center copyright">
+				<p>Powered by WordPress and Mystique theme by digitalnature | <a href="#">Подпишись!</a></p>
+			</div>
+		</div>
+	</footer>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+	<script src="js/jquery.matchHeight-min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
